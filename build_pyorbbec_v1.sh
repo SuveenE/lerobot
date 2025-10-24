bcadbe8b1e36366dc35224a1b2168a7bca9f4768
brycegoh <brycegoh96@gmail.com>
2025-10-24 12:22:52 +0800

    fix build

--- build_pyorbbec_v1.sh
+++ build_pyorbbec_v1.sh
@@ -1,5 +1,6 @@
 git clone https://github.com/orbbec/pyorbbecsdk.git
-git checkout main
+git fetch
+git switch main
 cd pyorbbecsdk
 pip3 install -r requirements.txt
 mkdir build
@@ -7,3 +8,7 @@ cd build
 cmake -Dpybind11_DIR=`pybind11-config --cmakedir` ..
 make -j4
 make install
+cd ../
+export PYTHONPATH=$PYTHONPATH:$(pwd)/install/lib/
+sudo bash ./scripts/install_udev_rules.sh
+sudo udevadm control --reload-rules && sudo udevadm trigger
