6962ffb5b4b90a5aaa11e9407bf343f65f6a2ddc
brycegoh <brycegoh96@gmail.com>
2025-11-12 14:30:29 +0800

    edit test script

--- examples/test_all_cameras.py
+++ examples/test_all_cameras.py
@@ -230,8 +230,8 @@ def capture_realsense_frames(serial_number: str, output_dir: str, num_frames: in
     COLOR_FORMAT = rs.format.rgb8
     DEPTH_FORMAT = rs.format.z16
     
-    config.enable_stream(rs.stream.color, WIDTH, HEIGHT, COLOR_FORMAT, FPS)
-    config.enable_stream(rs.stream.depth, WIDTH, HEIGHT, DEPTH_FORMAT, FPS)
+    config.enable_stream(rs.stream.color, 424, 240, COLOR_FORMAT, FPS)
+    config.enable_stream(rs.stream.depth, 424, 240, DEPTH_FORMAT, FPS)
     
     # Start pipeline
     pipeline = rs.pipeline()
