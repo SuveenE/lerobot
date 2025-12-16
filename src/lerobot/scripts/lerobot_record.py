ed74964dff4ea1e0fa00b16d8fd4691ee9d6b6c3
SuveenE <suveen.te1@gmail.com>
2025-12-16 20:54:03 +0800

    Make dataset private

--- src/lerobot/scripts/lerobot_record.py
+++ src/lerobot/scripts/lerobot_record.py
@@ -151,7 +151,7 @@ class DatasetRecordConfig:
     # Upload dataset to Hugging Face hub.
     push_to_hub: bool = True
     # Upload on private repository on the Hugging Face hub.
-    private: bool = False
+    private: bool = True
     # Add tags to your dataset on the hub.
     tags: list[str] | None = None
     # Number of subprocesses handling the saving of frames as PNG. Set to 0 to use threads only;
